8a7b968e68490143c19efffb8afc360f0f4e8f94
Shivangi1219 <shivangipsingh12@gmail.com>
2021-07-04 19:30:33 -0400

    Top menu bar finished

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import "./topbar.scss"
 import {Person,Mail} from "@material-ui/icons"
-function Topbar() {
+function Topbar({menuOpen, setMenuOpen}) {
     return (
-        <div className="topbar active">
+        <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
                     <a className="logo" href="#intro">Shivangi Singh</a>
@@ -18,7 +18,7 @@ function Topbar() {
                 </div>
 
                 <div className="right">
-                    <div className="hamburger">
+                    <div className="hamburger" onClick={()=> setMenuOpen(!menuOpen)}>
                         <span className="line1"></span>
                         <span className="line2"></span>
                         <span className="line3"></span>
